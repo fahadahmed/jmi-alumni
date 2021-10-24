1a52d9af762422c8e9b1b9e460c1fe06608ecb4d
Fahad Ahmed <fahad.ahmed@me.com>
2021-10-24 11:53:59 +1100

    JMI-4 about us page components

--- packages/web/src/pages/about.tsx
+++ packages/web/src/pages/about.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
+import Layout from '../components/Layout';
+import AboutJourney from '../journeys/about';
 
 function About() {
   return (
-    <div>About Us Page</div>
+    <Layout>
+      <AboutJourney />
+    </Layout>
   )
 }
 
