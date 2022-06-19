da97e265e293f24a536f79c3e4dc54bae32b1b8d
Fahad Ahmed <contact@fhdamd.dev>
2022-06-19 20:17:26 +1000

    added a blog journey to the project

---
+++ packages/web/src/pages/blog.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Layout } from '../components';
+import BlogJourney from '../journeys/blog';
+
+function Blog() {
+  return (
+    <Layout>
+      <BlogJourney />
+    </Layout>
+  )
+}
+
+export default Blog;
